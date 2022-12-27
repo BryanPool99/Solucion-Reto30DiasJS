c43bac64e1b518fe64bdd847f9d2b822436492f6
BryanPool99 <bryanpool991@hotmail.com>
2022-12-26 21:53:16 -0500

    dia 2 terminado

---
+++ Dia-1/dataTypes.js
@@ -0,0 +1,50 @@
+/*
+    5.Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+*/
+var Numberentero=4;
+var Numberfloat=4.5;
+var string="String";
+var booleanos=true
+var indefinidos=undefined;
+var nulos=null;
+console.log(typeof(Numberentero));
+/*
+    6.Declara cuatro variables sin asignar valores
+*/
+var primerVar;
+var segundoVar;
+var terceroVar;
+var cuartoVar;
+console.log(primerVar);
+/*
+    7.Declara cuatro variables con valores asignados
+*/
+var Numberentero=4;
+var Numberfloat=4.5;
+var string="String";
+var booleanos=true
+var indefinidos=undefined;
+console.log(string);
+/*
+    8.Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+*/
+var nombre="Bryan Pool";
+var apellido="Arredondo Ovalle";
+var estado_civil="Soltero";
+var país="Peru";
+var edad=23;
+/*
+    9.Declara variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea
+*/
+var nomb="Pool",
+ape="Ovalle",
+estado_Civil="Soltero",
+country="Arequipa",
+age=23;
+console.log(nomb,ape,estado_Civil,country,age);
+/*
+    10.Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+*/
+let miEdad=23,
+tuEdad=45;
+console.log("Tengo "+miEdad+" años.\nTú tienes "+tuEdad+" años.");
