588391601d9deda084ee6fbf0669bee46b902128
Bryan Arredondo <84214192+BryanPool99@users.noreply.github.com>
2023-01-02 12:54:52 -0500

    Delete Dia-1 directory

--- Dia-1/variable.js
+++
@@ -1,8 +0,0 @@
-/*
-    4.Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
-*/
-var string="String";
-var booleanos=true
-var indefinidos=undefined
-var nulos=null
-console.log(nulos);
