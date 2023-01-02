04a3fd2a6c2ee4a741bd5615ad9c591e661a157a
Bryan <bryanpool991@hotmail.com>
2023-01-02 12:48:27 -0500

    rename folder

---
+++ Dia-01-Intro/variable.js
@@ -0,0 +1,8 @@
+/*
+    4.Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+*/
+var string="String";
+var booleanos=true
+var indefinidos=undefined
+var nulos=null
+console.log(nulos);
