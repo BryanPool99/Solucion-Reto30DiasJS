4c9a9e8273697ba2de6f3720cde46031d460d1bb
Bryan <bryanpool991@hotmail.com>
2023-01-03 17:26:49 -0500

    Nivel 2 error,assert,warn  terminado

---
+++ Dia-13-ObjetoConsole/Nivel-2/Ejercicios.js
@@ -0,0 +1,13 @@
+/*
+    1. 10 > 2 * 10 use console.assert()
+*/
+console.assert(10 > 2 * 10,"Error 10 no es mayor que 2*10")
+/*
+    2.Escribe un mensaje de advertencia utilizando console.warn()
+*/
+console.warn("Este es una adventencia usando console.warm()")
+/*
+    3.Escribe un mensaje de error utilizando console.error()
+*/
+console.error("Este es un error usando el console.error()");
+
