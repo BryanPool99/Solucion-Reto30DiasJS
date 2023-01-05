550b55639e2a0c9b909cab8f96bee412f908b51b
BryanPool99 <bryanpool991@hotmail.com>
2023-01-04 19:46:38 -0500

    Nivel 3 de JSON.parse() terminado

---
+++ Dia-16-JSON/Nivel-3/Ejercicios.js
@@ -0,0 +1,114 @@
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`;
+/*
+    1.Parsear el txt JSON a objeto.
+*/
+const obj=JSON.parse(txt);
+console.log(obj);
+/*
+    2.Encuentra el usuario que tiene muchas habilidades de la variable almacenada en txt.
+*/
+let MostSkillUser;
+let maxSkill=0;
+for (const user in obj) {
+    const skills=obj[user].skills;
+    if(skills.length>maxSkill){
+        maxSkill=skills.length;
+        MostSkillUser=user;
+    }
+}
+console.log("El usuario con mas skills es "+MostSkillUser+" que tiene una cantidad de "+maxSkill);
