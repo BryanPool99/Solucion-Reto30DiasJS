ea8948bbe25a0291675a79d2416ed50f650b3c5e
Bryan <bryanpool991@hotmail.com>
2023-01-23 13:15:57 -0500

    funcionalidad de la pagina web terminada

--- Dia-28-TablaDePosiciones/LeaderBoard/assets/js/app.js
+++ Dia-28-TablaDePosiciones/LeaderBoard/assets/js/app.js
@@ -1,4 +1,3 @@
-console.log("hola");
 const error=document.querySelector(".error");
 const sectionPlayers=document.querySelector(".players");
 
@@ -10,7 +9,47 @@ const scoreInput=document.getElementById("score");
 
 //button para agregar datos al sectionPlayers
 const btnAdd=document.getElementById("btnAdd");
-
+function createContainerRow() {
+        var row=document.createElement("div");
+        row.className="row";
+        sectionPlayers.appendChild(row);
+        var containerFullDates=document.createElement("div");
+        containerFullDates.className="container-Full_Date";
+        var fullName=document.createElement("div");
+        fullName.className="fullName";
+        fullName.innerText=`${nameInput.value} ${lastnameInput.value}`;
+        containerFullDates.appendChild(fullName);
+        var date=document.createElement("div");
+        date.className="date";
+        var now=new Date();
+        date.innerHTML=formatDate(now);
+        containerFullDates.appendChild(date);
+        row.appendChild(containerFullDates);
+        var containerCountry=document.createElement("div");
+        containerCountry.className="container-country";
+        containerCountry.innerText=`${countryInput.value}`;
+        row.appendChild(containerCountry);
+        var containerScore=document.createElement("div");
+        containerScore.className="container-score";
+        var spanScore=document.createElement("span");
+        spanScore.innerText=`${scoreInput.value}`
+        containerScore.appendChild(spanScore)
+        row.appendChild(containerScore);
+        var containerBtns=document.createElement("div");
+        containerBtns.className="container-btns";
+        row.appendChild(containerBtns);
+        var buttonDelete=document.createElement("button");
+        buttonDelete.className="btnDelete";
+        containerBtns.appendChild(buttonDelete);
+        var buttonMore=document.createElement("button");
+        buttonMore.className="btnMore";
+        buttonMore.innerHTML="+5";
+        containerBtns.appendChild(buttonMore);
+        var buttonLess=document.createElement("button");
+        buttonLess.className="btnLess";
+        buttonLess.innerHTML="-5";
+        containerBtns.appendChild(buttonLess);
+}
 //funcion limpiar inputs
 function ClearInputs() {
     nameInput.value="";
@@ -35,87 +74,45 @@ function sortRows() {
 
     var players = document.querySelector(".players");
     rows.forEach(function(row) {
-        // row.classList.add('animate__animated', 'animate__fadeInDown');
         players.appendChild(row);
     });
 }
 //eventListener
 btnAdd.addEventListener("click",()=>{
     if(nameInput.value.length===0 || lastnameInput.value.length===0 || countryInput.value.length===0 || scoreInput.value.length===0){
-        error.classList.add("active")
+        error.classList.remove("animate__animated", "animate__heartBeat");
+        error.offsetWidth = error.offsetWidth; // esta linea es para forzar un reflow
+        error.classList.add("active");
+        error.classList.add('animate__animated', 'animate__heartBeat');
     }
     else{
         error.classList.remove("active");
-        //Creacion de estructura html de la section players usando JS
-        var row=document.createElement("div");
-        row.className="row";
-        sectionPlayers.appendChild(row);
-        var containerFullDates=document.createElement("div");
-        containerFullDates.className="container-Full_Date";
-        var fullName=document.createElement("div");
-        fullName.className="fullName";
-        fullName.innerText=`${nameInput.value} ${lastnameInput.value}`;
-        containerFullDates.appendChild(fullName);
-        var date=document.createElement("div");
-        date.className="date";
-        var now=new Date();
-        date.innerHTML=formatDate(now);
-        containerFullDates.appendChild(date);
-        row.appendChild(containerFullDates);
-        var containerCountry=document.createElement("div");
-        containerCountry.className="container-country";
-        containerCountry.innerText=`${countryInput.value}`;
-        row.appendChild(containerCountry);
-        var containerScore=document.createElement("div");
-        containerScore.className="container-score";
-        containerScore.innerText=`${scoreInput.value}`
-        row.appendChild(containerScore);
-        var containerBtns=document.createElement("div");
-        containerBtns.className="container-btns";
-        row.appendChild(containerBtns);
-        var buttonDelete=document.createElement("button");
-        buttonDelete.className="btnDelete";
-        buttonDelete.innerHTML=`<ion-icon name="trash-outline"></ion-icon>`;
-        containerBtns.appendChild(buttonDelete);
-        var buttonMore=document.createElement("button");
-        buttonMore.className="btnMore";
-        buttonMore.innerText="+5";
-        containerBtns.appendChild(buttonMore);
-        var buttonLess=document.createElement("button");
-        buttonLess.className="btnLess";
-        buttonLess.innerText="-5";
-        containerBtns.appendChild(buttonLess);
+        createContainerRow();
         ClearInputs();
     } 
-    var btnsDelete=document.querySelectorAll(".btnDelete");
-    btnsDelete.forEach((btnD)=>{
-        btnD.addEventListener("click",(event)=>{
-            event.currentTarget.closest('.row').remove();
-        })
-    })
-    var btnsMore=document.querySelectorAll(".btnMore");
-    btnsMore.forEach((btnM)=>{
-        btnM.addEventListener("click",(e)=>{
-            let score= e.currentTarget.closest('.row').querySelector('.container-score');
-            score.classList.add('animate__animated', 'animate__fadeInDownBig');
-            let newScore=parseInt(score.textContent);
-            score.textContent=newScore+5;
-        })
-    })
-    btnsMore.forEach((e)=>{
-        e.addEventListener("click",sortRows);
-    })
-    var btnsLess=document.querySelectorAll(".btnLess");
-    btnsLess.forEach((btnL)=>{
-        btnL.addEventListener("click",(e)=>{
-            let score= e.currentTarget.closest('.row').querySelector('.container-score').classList.add('animate__animated', 'animate__fadeInUpBig');
-            let newScore=parseInt(score.textContent);
-            score.textContent=newScore-5;
-        })
-    })
-    btnsLess.forEach((e)=>{
-        e.addEventListener("click",sortRows)
-    })
-
+    sortRows();
 })
-btnAdd.addEventListener("click",sortRows);
+sectionPlayers.addEventListener("click",(e)=>{
+    if (e.target) {
+        if (e.target.className=="btnDelete" || e.target.className=="hydrated") {
+            e.target.closest(".row").classList.add("animate__animated","animate__hinge");
+            setTimeout(function() {
+                e.target.closest('.row').remove();
+            }, 2000);
+            //e.target.closest('.row').remove();
+            sortRows();
+
+        }else if(e.target.className=="btnMore"){
+            let score= e.target.closest('.row').querySelector('.container-score span');
+            let currentScore=parseInt(score.innerHTML);
+            score.innerHTML=currentScore+5;
+            sortRows();
+
+        }else if(e.target.className=="btnLess"){
+            let score= e.target.closest('.row').querySelector('.container-score span');
+            let currentScore=parseInt(score.innerHTML);
+            score.innerHTML=currentScore-5;
+            sortRows();
+        }
+    }
+})
